558eafa0177b4ac0c4a9015eb4ddd8651b9583f8
azrap <azraap@gmail.com>
2019-04-02 10:55:43 -0700

    answering challenge questions in the callback.js file - no stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,23 +27,32 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length-1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  sumxy=x+y;
+  cb(sumxy); // can also just do cb(x+y);
+
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  multxy=x*y;
+  cb(multxy); 
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  bool=list.includes(item);
+  cb(bool);
 }
 
 /* STRETCH PROBLEM */
